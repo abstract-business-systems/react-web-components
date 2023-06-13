469951864ddb6002771e85da48b2ffbf85bbc733
pdn-abs <pdn@abstractit.in>
2023-06-13 21:12:01 +0530

    improved storybook controls for videoPlayer Component

--- src/stories/VideoPlayer.stories.js
+++ src/stories/VideoPlayer.stories.js
@@ -6,6 +6,38 @@ const url = video;
 const component = {
 	title: 'Display/Video Player',
 	component: ReactVideoPlayer,
+	argTypes: {
+		status: {
+			type: 'select',
+			options: [
+				'playing',
+				'paused',
+			],
+		},
+		mode: {
+			type: 'select',
+			options: [
+				'light',
+				'normal',
+			],
+		},
+		volume: {
+			control: {
+				type: 'range',
+				min: 0,
+				max: 1,
+				step: 0.1,
+			},
+		},
+		playbackRate: {
+			control: {
+				type: 'range',
+				min: 0.25,
+				max: 2,
+				step: 0.25,
+			},
+		},
+	},
 };
 
 export default component;
