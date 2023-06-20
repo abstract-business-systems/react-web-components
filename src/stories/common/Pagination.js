7e3f4ca3ec74cf29289d065298bf307a1c288beb
nal-abs <nal@abstractit.in>
2023-06-20 12:01:29 +0530

    Introduced onChange in pagination.

--- src/stories/common/Pagination.js
+++ src/stories/common/Pagination.js
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import { Pagination as MuiPagination } from '@mui/material';
+import buildEvent from './helper/buildEvent';
+import { nothing } from '@laufire/utils/predicates';
 
-const Pagination = (args) => <MuiPagination { ...args }/>;
+const Pagination = ({ onChange = nothing, ...rest }) =>
+	<MuiPagination { ...{
+		onChange: (evt, newValue) =>
+			onChange(buildEvent({ newValue })), ...rest,
+	} }
+	/>;
 
 export default Pagination;
