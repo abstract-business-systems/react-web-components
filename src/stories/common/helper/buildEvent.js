a0e41b5642f267aaaaa60ce5aa85f10b5d2fe2af
nal-abs <nal@abstractit.in>
2023-06-21 11:16:29 +0530

    Added meta in buildEvent object.

--- src/stories/common/helper/buildEvent.js
+++ src/stories/common/helper/buildEvent.js
@@ -1,8 +1,12 @@
 
-const buildEvent = ({ isValid = true, userInput, newValue }) => ({
+const buildEvent = ({
+	isValid = true, userInput,
+	newValue, status = 'success',
+}) => ({
 	target: {
 		value: isValid ? newValue : userInput.valid,
 		error: isValid ? null : { message: 'IncorrectEntry!' },
+		meta: { status },
 	},
 });
 
