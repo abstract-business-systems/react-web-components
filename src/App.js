1feaf49ed566200285c34482f248355072a2033f
nal-abs <nal@abstractit.in>
2023-07-01 12:29:05 +0530

    cleaned unused App module.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,6 @@
 import React from 'react';
 import './App.scss';
-import Tab from '../src/stories/common/Tab/index';
 
-const getTabProp = (context) => ({
-	...context,
-	contents: {
-		Journal: {
-			label: 'Journal',
-			component: 'Journal',
-			props: { ...{ ...context, data: { collection: 'journals' }}},
-		},
-		Ledger: {
-			label: 'Ledger',
-			component: 'Ledger',
-			props: { ...{ ...context, data: { collection: 'ledgers' }}},
-		},
-	},
-	value: 'Journal',
-	style: 'textOnly',
-});
-
-const App = (context) => <div className="App">
-	<Tab { ...getTabProp(context) }/>
-</div>;
+const App = () => <div className="App"/>;
 
 export default App;
