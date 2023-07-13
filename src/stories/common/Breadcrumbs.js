d2c329414069a1811bcc2b16c5fbd523a864fa03
njj-abs <njj@abstractit.in>
2023-07-13 10:56:47 +0530

    Improved the breadcrumbs.

--- src/stories/common/Breadcrumbs.js
+++ src/stories/common/Breadcrumbs.js
@@ -4,13 +4,21 @@ import Link from './Link';
 
 const Breadcrumbs = ({ value, ...args }) =>
 	<MuiBreadcrumbs aria-label="breadcrumb" { ...args }>
-		{ value.map(({ href, children }, key) =>
-			<Link
-				key={ key }
-				{ ...{ underline: 'hover', color: 'inherit', href: href } }
-			>
-				{ children }
-			</Link>) }
+		{ value.map(({ children, ...rest }, key) => {
+			const last = key === (value.length - 1);
+
+			return (
+				<Link
+					key={ key }
+					{ ...{
+						underline: 'hover',
+						color: last ? 'text.primary' : 'inherit',
+						...rest,
+					} }
+				>
+					{ children }
+				</Link>);
+		}) }
 	</MuiBreadcrumbs>;
 
 export default Breadcrumbs;
