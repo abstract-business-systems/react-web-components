8f47a7c3974202d92b883085f821dcd632a34115
njj-abs <njj@abstractit.in>
2023-08-07 10:27:36 +0530

    Created the provider in Document.

--- src/components/Navigation/Document.js
+++ src/components/Navigation/Document.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Section from './Section';
 import { useLocation } from 'react-router-dom';
 import { unique } from '@laufire/utils/predicates';
+import { NavContext } from './NavContext';
+import { merge } from '@laufire/utils/collection';
 
-const Document = ({ children, name = '' }) => {
+// eslint-disable-next-line max-lines-per-function
+const Document = ({ children }) => {
 	const { pathname } = useLocation();
-	const location = pathname.split('/').filter(unique)
+	const currLocation = pathname.split('/').filter(unique)
 		.reduce((acc, curr) => acc.concat({
 			path: `${ acc[acc.length - 1]?.path || '' }${ curr }/`,
 			label: curr,
 		}), []);
 
-	return <Section { ...{ name, location } }>{ children }</Section>;
+	const data = {
+		parentPath: '',
+		options: {},
+		location: [],
+	};
+
+	const onLoad = (option) => {
+		merge(data, option);
+	};
+
+	const value = useMemo(() => ({ onLoad, data }));
+
+	return <NavContext.Provider value={ value }>
+		<Section { ...{ name: '', location: currLocation, parentPath: '' } }>
+			{ children }</Section>
+	</NavContext.Provider>;
 };
 
 export default Document;
--- src/components/Navigation/Section.js
+++ src/components/Navigation/Section.js
@@ -1,50 +1,49 @@
-/* eslint-disable react/destructuring-assignment */
-import React, { useContext, useMemo } from 'react';
+/* eslint-disable max-lines-per-function */
+import React from 'react';
 import { NavContext } from './NavContext.js';
-import { merge } from '@laufire/utils/collection.js';
 import scaffold from './helper/scaffold.js';
-import { useNavigate } from 'react-router-dom';
 
-// eslint-disable-next-line max-lines-per-function, complexity
-const Section = ({ children, name, pathname, location, label }) => {
-	const level = useContext(NavContext);
+const Section = ({ children, name, parentPath = '', label }) => {
+	const currPath = `${ parentPath }${ name }/`;
+	// const currLocation = location
+	// 	|| level.location.find((data) => data.path === path);
 
-	const path = `${ level.path }${ name }/`;
-	const currLocation = location
-		|| level.location.find((data) => data.path === path);
-
-	const navigate = useNavigate();
+	// const navigate = useNavigate();
 
 	return (
-		<section className="section">
-			<NavContext.Provider
-				value={ useMemo(() => ({
-					pathname: pathname || level.pathname,
-					location: location || level.location,
-					value: level.value + 1,
-					path: path,
-					options: merge(level.options,
-						scaffold(path.split('/')
-							.map((data) => (data ? `/children/${ data }` : data))
-							.join(''),
-						{
-							children: {},
-							name: name,
-							label: label || name,
-						})),
-				}),
-				[name, location, level.location, pathname, level.pathname]) }
-			>
+		<NavContext.Consumer>
+			{ (context) => {
+				context.onLoad({
+					parentPath: currPath,
+					options: scaffold(currPath.split('/').map((data) =>
+						(data ? `/children/${ data }` : data))
+						.join(''),
+					{
+						children: {},
+						name: name,
+						label: label || name,
+						path: currPath,
+					}),
+				});
+
+				return <section className="section">
+					{ React.Children.map(children, (child) =>
+						(child.type.name === 'Section'
+							? React.cloneElement(child,
+								{ parentPath: currPath })
+							: child)) }
+					{ /*
 				{ React.Children.map(children, (child) =>
 					(child.type.name === 'Section'
 						? React.cloneElement(child)
 						: currLocation && child)) }
-				{ /* eslint-disable-next-line jsx-a11y/anchor-is-valid */ }
 				<a onClick={ () => { navigate(`${ level.path }${ name }/`); } }>
 					{ `${ level.path }${ name }/` }
 				</a>
-			</NavContext.Provider>
-		</section>
+			*/ }
+				</section>;
+			} }
+		</NavContext.Consumer>
 	);
 };
 
