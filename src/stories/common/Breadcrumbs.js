9659aaf32a2cfe6f9359e42331b647ad23e421b7
njj-abs <njj@abstractit.in>
2023-08-09 11:45:28 +0530

    Changed breadcrumbs by useNavigate.

--- src/stories/common/Breadcrumbs.js
+++ src/stories/common/Breadcrumbs.js
@@ -2,26 +2,35 @@ import React from 'react';
 import MuiBreadcrumbs from '@mui/material/Breadcrumbs';
 import Link from './Link';
 import { Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-const Breadcrumbs = ({ value = [], ...args }) =>
-	<MuiBreadcrumbs aria-label="breadcrumb" { ...args }>
-		{ value.map(({ label, path, ...rest }, key) => {
+const Breadcrumb = ({ last, navigate, path, label, ...rest }) =>
+	<Link
+		{ ...{
+			underline: 'hover',
+			color: last ? 'text.primary' : 'inherit',
+			...last && { component: Typography },
+			onClick: () => navigate(path),
+			...rest,
+		} }
+	>
+		{ label }
+	</Link>;
+
+const Breadcrumbs = ({ value = [], ...args }) => {
+	const navigate = useNavigate();
+
+	return <MuiBreadcrumbs aria-label="breadcrumb" { ...args }>
+		{ value.map((data, key) => {
 			const last = key === (value.length - 1);
 
 			return (
-				<Link
+				<Breadcrumb
 					key={ key }
-					{ ...{
-						underline: 'hover',
-						color: last ? 'text.primary' : 'inherit',
-						...last && { component: Typography },
-						href: path,
-						...rest,
-					} }
-				>
-					{ label }
-				</Link>);
+					{ ...{ ...data, last, navigate } }
+				/>);
 		}) }
 	</MuiBreadcrumbs>;
+};
 
 export default Breadcrumbs;
