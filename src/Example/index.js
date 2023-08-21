dd2fe6bc92d0c0b5dc04bedc2c05ea9715f49959
njj-abs <njj@abstractit.in>
2023-08-21 10:27:03 +0530

    Introduced the SelectWithState.

--- src/Example/index.js
+++ src/Example/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import InputWithState from '../components/withState/InputWithState';
+import SelectWithState from '../components/withState/SelectWithState';
 import Document from '../components/Document';
+
+const selectProps = {
+	value: 'b/c/d',
+	multiple: true,
+	options: [
+		{
+			value: 'ten',
+			label: 'Ten',
+		}, {
+			value: 'twenty',
+			label: 'Twenty',
+		}, {
+			value: 'thirty',
+			label: 'Thirty',
+		},
+	],
+};
 
 const Example = () =>
 	<Document { ...{
@@ -12,6 +30,7 @@ const Example = () =>
 	} }
 	>
 		<InputWithState value="a/b/c"/>
+		<SelectWithState { ...selectProps }/>
 	</Document>;
 
 export default Example;
---
+++ src/components/withState/SelectWithState.js
@@ -0,0 +1,6 @@
+import withState from '.';
+import Select from '../Select';
+
+const SelectWithState = withState(Select);
+
+export default SelectWithState;
