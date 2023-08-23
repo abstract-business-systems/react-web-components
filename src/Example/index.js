bb135e254c6c988ce1c7eb277d8eb7952acddb8d
njj-abs <njj@abstractit.in>
2023-08-23 16:53:22 +0530

    Refactored.

--- src/Example/index.js
+++ src/Example/index.js
@@ -1,47 +1,7 @@
 import React from 'react';
-import { Input, Select } from '../components/WithState';
-import Document from '../components/Document';
-import Section from '../components/Section';
-import RESTClient from '../components/RESTClient';
-
-const selectProps = {
-	value: '/b/c/d/',
-	multiple: '/multiple/',
-	action: 'patch',
-	options: [
-		{
-			value: 'ten',
-			label: 'Ten',
-		}, {
-			value: 'twenty',
-			label: 'Twenty',
-		}, {
-			value: 'thirty',
-			label: 'Thirty',
-		},
-	],
-};
-
-const inputProps = {
-	value: '/a/b/c/',
-	action: 'patch',
-};
+import WithState from './WithState';
 
 const Example = () =>
-	<Document { ...{
-		initialState: {
-			a: { b: { c: '' }},
-			b: { c: { d: [] }},
-			todo: [{ a: 1 }],
-			multiple: false,
-		},
-	} }
-	>
-		<Input { ...inputProps }/>
-		<Select { ...selectProps }/>
-		<Section label="ParentOne" name="parentOne">
-			<RESTClient name="apiClient" base="url"/>
-		</Section>
-	</Document>;
+	<WithState/>;
 
 export default Example;
