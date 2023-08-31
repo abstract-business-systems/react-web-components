08f7225928dc4da9cc08e9bf690f99746922848a
nal-abs <nal@abstractit.in>
2023-08-31 13:37:38 +0530

    Added default variant.

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { Button as MuiButton } from '@mui/material';
 import getIcons from './common/helper/getIcons';
 
-const Button = ({ children = 'Button', startIcon, endIcon, ...rest }) =>
-	<MuiButton { ...{ ...rest, ...getIcons({ startIcon, endIcon }) } }>
+const Button = ({
+	children = 'Button', variant = 'contained',
+	startIcon, endIcon, ...rest
+}) =>
+	<MuiButton { ...{ variant, ...rest, ...getIcons({ startIcon, endIcon }) } }>
 		{ children }
 	</MuiButton>;
 
