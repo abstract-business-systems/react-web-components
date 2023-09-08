48d9a6571ebe93cd06933ae3545047c31c6091a7
Karpagam-Krishnasamy <karpagamkrishnasamy@gmail.com>
2023-09-08 12:02:11 +0530

    Marked scroll positions in event.

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable max-lines-per-function */
+/* eslint-disable no-magic-numbers */
 import React, { useEffect, useRef } from 'react';
 import { useResizeDetector } from 'react-resize-detector';
 import { Box } from '@mui/material';
@@ -10,9 +12,11 @@ import buildEvent from '../common/helper/buildEvent';
 
 const getArgs = ({ className, ...args }) => ({
 	...args, tabIndex: '-1',
+	style: { height: '1000px', overflow: 'auto' },
 	className: clsx('absContainer', className),
 });
 
+// eslint-disable-next-line max-lines-per-function
 const Container = ({
 	onChange = identity, children,
 	shortcuts = [], resize, ...args
@@ -20,13 +24,39 @@ const Container = ({
 	const ref = useRef();
 	const { width, height } = useResizeDetector({ targetRef: ref, ...resize });
 
-	useEffect(() => {
+	const handleChange = () => {
+		const { current }	= ref;
 		const orientation = width < height ? 'portrait' : 'landscape';
+		const {
+			scrollHeight,
+			scrollWidth,
+			scrollTop,
+			scrollLeft,
+			clientWidth,
+			clientHeight,
+		} = current;
 
-		width && onChange(buildEvent({ value: { width, height, orientation }}));
-	}, [width, height]);
+		onChange(buildEvent({
+			value: {
+				width: width || clientWidth,
+				height: height || clientHeight,
+				orientation: orientation,
+				scroll: {
+					width: scrollWidth,
+					height: scrollHeight,
+					x: scrollLeft,
+					y: scrollTop,
+				},
+			},
+		}));
+	};
 
 	useEffect(() => {
+		handleChange();
+		const { current: element } = ref;
+
+		window.addEventListener('resize', handleChange);
+		element.addEventListener('scroll', handleChange);
 		map(shortcuts, (cb, key) => shortcut.createShortcut({ cb, key, ref }));
 	}, []);
 
