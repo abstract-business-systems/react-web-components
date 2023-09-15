f49c44d2ce46b7d6855b89ec1bbcbb15c021073f
njj-abs <njj@abstractit.in>
2023-09-15 19:16:57 +0530

    Bug fixed.

--- src/components/WithContext/Button.js
+++ src/components/WithContext/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button as ButtonWithState } from '../WithState';
 
-const Button = ({ onClick, value, props }) =>
+const Button = ({ onClick, value, ...props }) =>
 	<ButtonWithState { ...{
 		...props,
 		onClick: { ...onClick, data: value },
