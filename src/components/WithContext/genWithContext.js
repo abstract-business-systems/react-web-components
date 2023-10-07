ce380e2eb94dcf6c84a91c421489ed7f1b4662be
njj-abs <njj@abstractit.in>
2023-10-07 10:59:19 +0530

    Improved.

--- src/components/WithContext/genWithContext.js
+++ src/components/WithContext/genWithContext.js
@@ -2,13 +2,14 @@ import React from 'react';
 import GlobalContext from '../Document/GlobalContext';
 import { result } from '@laufire/utils/collection';
 import { resolve } from '@laufire/utils/path';
+import { defined } from '@laufire/utils/fn';
 
 // eslint-disable-next-line react/display-name
 const genWithContext = ({ Component }) => ({ ...props }) =>
 	<GlobalContext.Consumer>
 		{ (context) => {
 			const value = result(context.state,
-				resolve(context.valuePath, props.value || ''));
+				resolve(context.valuePath, defined(props.value, '')));
 
 			return (
 				<Component { ...{ ...props, value } }/>
