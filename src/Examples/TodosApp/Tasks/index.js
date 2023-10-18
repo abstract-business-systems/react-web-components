9f82ab80100e0f2a1c07f5629073259f187f3953
njj-abs <njj@abstractit.in>
2023-10-18 13:33:59 +0530

    Moved the Task Header.

--- src/Examples/TodosApp/Tasks/index.js
+++ src/Examples/TodosApp/Tasks/index.js
@@ -2,10 +2,11 @@ import { Box } from '@mui/material';
 import React from 'react';
 import TasksDisplay from './TasksDisplay';
 import TaskRESTClient from './TaskRESTClient';
+import Header from './Header';
 
 const Tasks = () =>
 	<Box>
-		<Box textAlign="center">Tasks</Box>
+		<Header/>
 		<TaskRESTClient/>
 		<TasksDisplay/>
 	</Box>;
